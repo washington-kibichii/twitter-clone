7730618cee2a899f64040b5df0c05825457f48c5
Washington <washington.kibichii@radioafricagroup.co.ke>
2021-01-21 02:09:54 +0300

    Feed component

---
+++ src/Feed.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TweetBox from './TweetBox';
+import './Feed.css';
+
+
+function Feed() {
+    return (
+        <div className="feed">
+        {/* Header */}
+        <div className="feed__header">
+            <h2>Home</h2>
+        </div>
+        
+        {/* Tweetbox */}
+        <TweetBox />
+
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        {/* Post */}
+        </div>
+    )
+}
+
+export default Feed
