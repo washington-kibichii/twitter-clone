08d7aee058de6282c96c94e6aec3ea93abceca78
Washington <washington.kibichii@radioafricagroup.co.ke>
2021-01-21 02:10:18 +0300

    App styling, setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import Feed from './Feed';
 import Sidebar from './Sidebar';
+import Widgets from './Widgets';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hey Super Developer!!! Lets get Super pumped building</h1>
       {/* {Sidebar} */}
       <Sidebar />
+
       {/* {Feed} */}
+      <Feed />
+
       {/* {Widgets} */}
+      <Widgets />
     </div>
   );
 }
